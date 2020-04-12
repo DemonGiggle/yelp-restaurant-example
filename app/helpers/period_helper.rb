ca84788ff85de503ce8e2178100d53de997364c7
gigo <demoncris@gmail.com>
2020-04-12 10:20:07 +0800

    Display period description in restaurants index

---
+++ app/helpers/period_helper.rb
@@ -0,0 +1,30 @@
+module PeriodHelper
+  def periods_in_human_readable_form(restaurant)
+    if restaurant.periods.blank?
+      "No periods found"
+    else
+      str=restaurant.periods.map do |p|
+        wdays = p.weekdays.map do |w|
+          weekday_to_human_readable_form(w)
+        end.join(" ")
+
+        "Weekday: #{wdays}<br>(UTC) Open: #{p.hour_start} Close: #{p.hour_end}"
+      end.join("<br>")
+
+      sanitize str
+    end
+  end
+
+  def weekday_to_human_readable_form(wday)
+    mapping = {
+      0 => "Sun",
+      1 => "Mon",
+      2 => "Tue",
+      3 => "Wed",
+      4 => "Thu",
+      5 => "Fri",
+      6 => "Sat",
+    }
+    mapping[wday]
+  end
+end
